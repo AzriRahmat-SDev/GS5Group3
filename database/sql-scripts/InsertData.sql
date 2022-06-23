3e77544fa63e74555dd0a0ddc90f52d314ff8d1f
Victor Lin <linvictorw@gmail.com>
2022-06-23 09:48:06 +0800

    fix sql script for bookings table

--- database/sql-scripts/InsertData.sql
+++ database/sql-scripts/InsertData.sql
@@ -1,28 +1,7 @@
-<<<<<<< Updated upstream
 INSERT INTO bookings (PlotID, UserID, StartDate, EndDate, LeaseCompleted) VALUES ('000001', '000001', '2022-04-23', '2022-05-23', 'true');
 INSERT INTO bookings (PlotID, UserID, StartDate, EndDate, LeaseCompleted) VALUES ('000002', '000002', '2022-03-09', '2022-04-09', 'true');
 INSERT INTO bookings (PlotID, UserID, StartDate, EndDate, LeaseCompleted) VALUES ('000003', '000003', '2022-04-20', '2022-05-20', 'false');
 INSERT INTO bookings (PlotID, UserID, StartDate, EndDate, LeaseCompleted) VALUES ('000004', '000004', '2022-04-22', '2022-05-22', 'false');
-=======
-INSERT INTO
-    bookings (PlotID, UserID, StartDate, EndDate)
-VALUES
-    ('000001', '000001', '2022-04-23', '2022-05-23');
-
-INSERT INTO
-    bookings (PlotID, UserID, StartDate, EndDate)
-VALUES
-    ('000002', '000002', '2022-03-09', '2022-04-09');
-
-INSERT INTO
-    bookings (PlotID, UserID, StartDate, EndDate)
-VALUES
-    ('000003', '000003', '2022-04-20', '2022-05-20');
-
-INSERT INTO
-    bookings (PlotID, UserID, StartDate, EndDate)
-VALUES
-    ('000004', '000004', '2022-04-22', '2022-05-22');
 
 INSERT INTO
     users (Name, Username, Password, Email)
@@ -5003,4 +4982,3 @@ VALUES
         'Choa Chu Kang Park',
         'Choa Chu Kang Dr, Singapore 688845'
     );
->>>>>>> Stashed changes
